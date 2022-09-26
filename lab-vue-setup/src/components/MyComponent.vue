2274508ff6f2168842800eee229bcc307ccdf676
Raquel Almarcha <ralmarcha@gmail.com>
2022-09-26 18:07:48 +0200

    first commit

---
+++ lab-vue-setup/src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ myText }}</h1>
+    <button @click="helloWorld">Click ME</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let myText = ref("This is my text interpolation");
+
+let app = new Vue({
+  methods: {
+    helloWorld() {
+      console.log("Hello");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
